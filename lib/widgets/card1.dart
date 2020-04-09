dcfc49c7ab1fade77251c160ad80c04871909f4a
Yash Sahu <yash.18je0953@ece.iitism.ac.in>
2020-04-10 00:02:19 +0530

    Changed some alignment issues of card1 Widget

--- lib/widgets/card1.dart
+++ lib/widgets/card1.dart
@@ -7,11 +7,17 @@ Widget card1({@required BuildContext context, String productId, String purchaseD
   if (logo == null) logo = "https://logosvector.net/wp-content/themes/iLoveLogos/img/not-available.jpg";
   if (subTotal == null) subTotal = "0";
   if(displayCity == null) displayCity = "Can't find";
+
+  var maxHeight = 150.0;
+  var smallFont = 12.0;
+  var imgHeightWidth = 90.0;
+  var titleSize = 18.0;
+
   return Padding(
     padding: const EdgeInsets.all(8.0),
     child: Container(
       constraints: BoxConstraints(
-        maxHeight: 120.0,
+        maxHeight: maxHeight,
       ),
       color: Colors.white,
       child: Column(
@@ -20,12 +26,11 @@ Widget card1({@required BuildContext context, String productId, String purchaseD
           Container(
             padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
             child: Row(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                      child: Row(
+                Container(
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
                       SizedBox(
@@ -33,7 +38,7 @@ Widget card1({@required BuildContext context, String productId, String purchaseD
                       ),
                       Text(
                         productId,
-                        style: TextStyle(color: Colors.black54, fontSize: 13),
+                        style: TextStyle(color: Colors.black54, fontSize: smallFont),
                       ),
                       SizedBox(
                         width: 20,
@@ -42,139 +47,135 @@ Widget card1({@required BuildContext context, String productId, String purchaseD
                         purchaseDate[6]+purchaseDate[7]+" - "+purchaseDate[4]+purchaseDate[5]+" - "+purchaseDate[0]+purchaseDate[1]+purchaseDate[2]+purchaseDate[3],
                         style: TextStyle(
                           color: Colors.black54,
-                          fontSize: 13,
+                          fontSize: smallFont,
                         ),
                       )
                     ],
-                  )),
+                  )
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.grey[800],
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(15),
-                              topLeft: Radius.circular(15),
-                            )),
-                        child: Text(
-                          'FOOD & DRINKS',
-                          style: TextStyle(fontSize: 10),
-                        ),
-                        padding: EdgeInsets.fromLTRB(30, 5, 15, 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[800],
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(15),
+                            topLeft: Radius.circular(15),
+                          )),
+                      child: Text(
+                        'FOOD & DRINKS',
+                        style: TextStyle(fontSize: 10),
                       ),
-                    ],
-                  ),
+                      padding: EdgeInsets.fromLTRB(30, 5, 15, 5),
+                    ),
+                  ],
                 )
               ],
             ),
           ),
           Container(
-            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 5),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Expanded(
-                  flex: 2,
-                  child: Image(
-                    image: NetworkImage(logo),
-                    height: 90,
-                    width: 90,
-                  ),
-                ),
-                Expanded(
-                  flex: 7,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(15.0, 0, 0, 0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          displayName,
-                          style:
-                              TextStyle(color: Colors.grey[900], fontSize: 18),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Icon(
-                              Icons.location_on,
-                              color: Colors.black54,
-                              size: 12,
-                            ),
-                            Text(
-                              displayCity,
-                              style: TextStyle(
-                                color: Colors.black54,
-                                fontSize: 12,
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Image(
+                      image: NetworkImage(logo),
+                      height: imgHeightWidth,
+                      width: imgHeightWidth,
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15.0, 0, 0, 0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            displayName,
+                            style:
+                            TextStyle(color: Colors.grey[900], fontSize: titleSize),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Icon(
+                                Icons.location_on,
+                                color: Colors.black54,
+                                size: smallFont,
+                              ),
+                              Text(
+                                displayCity,
+                                style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: smallFont,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  flex: 4,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            subTotal,
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/billPage');
+                      },
+                      child: Container(
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Text(
-                              subTotal,
+                              'VIEW RECEIPT',
                               style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600),
+                                fontSize: smallFont,
+                                color: Colors.grey[700],
+                                decoration: TextDecoration.underline,
+                              ),
                             ),
-                            SizedBox(
-                              width: 10,
-                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 12,
+                              color: Colors.grey[700],
+                            )
                           ],
                         ),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/billPage');
-                        },
-                        child: Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                'VIEW RECEIPT',
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.grey[700],
-                                  decoration: TextDecoration.underline,
-                                ),
-                              ),
-                              Icon(
-                                Icons.arrow_forward_ios,
-                                size: 12,
-                                color: Colors.grey[700],
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 )
               ],
             ),
